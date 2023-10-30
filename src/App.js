765adeac9845b50ca01cdc33859c8cfad9f68755
rChinnnn <b10121035@yuntech.edu.tw>
2023-10-31 00:22:07 +0800

    chapter 3-5

--- src/App.js
+++ src/App.js
@@ -21,6 +21,74 @@ function App() {
       
       // const ensAddress = await provider.lookupAddress(address);
       // console.log(ensAddress); // only available in mainnet
+      const contractAddr = '0xC080004ea96f2daeDCE1C0d7E81B3526C120f721'
+      const abi = [
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "str",
+              "type": "string"
+            }
+          ],
+          "name": "store",
+          "outputs": [],
+          "stateMutability": "nonpayable",
+          "type": "function"
+        },
+        {
+          "inputs": [
+            {
+              "internalType": "string",
+              "name": "str",
+              "type": "string"
+            }
+          ],
+          "name": "storePaidMsg",
+          "outputs": [],
+          "stateMutability": "payable",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "message",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        {
+          "inputs": [],
+          "name": "retrievePaidMsg",
+          "outputs": [
+            {
+              "internalType": "string",
+              "name": "",
+              "type": "string"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        }
+      ]
+      const contract = new ethers.Contract(contractAddr, abi, signer);
+      let tx = await contract.store("Free fish!")
+      await tx.wait()
+      let msg = await contract.message()
+      console.log(msg)
+
+      let payEtherAmount = ethers.parseEther("0.00001");
+      let ptx = await contract.storePaidMsg(
+        "Paid fish!",
+        {value: payEtherAmount}
+      )
+      let response = await ptx.wait()
+      console.log(response)
     }
     init()
   }, [])
